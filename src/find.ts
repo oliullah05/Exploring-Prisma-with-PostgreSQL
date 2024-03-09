572ab42e161fcb6382ed3507824803b1d6638292
oliullah05 <mdolihasan@gmail.com>
2024-03-09 13:15:27 +0600

    Upsert Operations And Select Queries

--- src/find.ts
+++ src/find.ts
@@ -3,9 +3,20 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const main = async()=>{
 // find all
+
+const findAll = await prisma.post.findMany({
+    select:{
+        authorName:true
+    }
+})
+
+
+
+// find first
+
 const findFirst =await prisma.post.findFirstOrThrow({
     where:{
-        id:2
+        id:45
     }
 })
 
@@ -13,7 +24,12 @@ const findFirst =await prisma.post.findFirstOrThrow({
 // find unique
 const findUnique =await prisma.post.findUniqueOrThrow({
     where:{
-        id:200
+        id:42
+    },
+    select:{
+        title: false,
+    content: true,
+    // authorName: true,
     }
 })
 
@@ -22,7 +38,7 @@ const findUnique =await prisma.post.findUniqueOrThrow({
 
 
 const getAllDataFromDB = await prisma.post.findMany()
-console.log({findUnique});
+console.log({findAll});
 
 }
 
--- src/update.ts
+++ src/update.ts
@@ -2,32 +2,52 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
-const updates = async()=>{
-// single update
+const updates = async () => {
+    // single update
 
-// const singleUpdate = await prisma.post.update({
-//     where:{
-//         id:2
-//     },
-//     data:{
-//         authorName:"1",
-       
-//     }
-// })
+    // const singleUpdate = await prisma.post.update({
+    //     where:{
+    //         id:2
+    //     },
+    //     data:{
+    //         authorName:"1",
+
+    //     }
+    // })
 
 
 
-// update many data
-const updateMany = await prisma.post.updateMany({
-    where:{
-        title:"This is tittle"
-    },
-    data:{
-        title:"This is tittle 55"
-    }
-})
+    // update many data
+    // const updateMany = await prisma.post.updateMany({
+    //     where:{
+    //         title:"This is tittle"
+    //     },
+    //     data:{
+    //         title:"This is tittle 55"
+    //     }
+    // })
 
-console.log(updateMany);
+
+    // update or insert
+
+
+    const upsertData = await prisma.post.upsert({
+        where: {
+            id: 600
+        },
+        update: {
+            title: "updated tittle 6",
+            authorName:"oli 43"
+        },
+        create: {
+            title: "title 1",
+            content:"content 1",
+          
+        }
+    })
+
+
+    console.log(upsertData);
 }
 
 updates()
