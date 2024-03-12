bd90ba84a98bef6214e849843046bc580f861a65
oliullah05 <mdolihasan@gmail.com>
2024-03-12 19:44:14 +0600

    Sum, Count, Maximum, Minimum Aggregation

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -1,15 +1,54 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma =new PrismaClient()
+const prisma = new PrismaClient()
 
-const aggregates = async()=>{
-const avarageAge= await prisma.user.aggregate({
-    _avg:{
-        age:true
+const aggregates = async () => {
+
+    // avarage age
+    const avarageAge = await prisma.user.aggregate({
+        _avg: {
+            age: true
+        }
+    })
+
+// sum of ages
+
+const sumOfAges = await prisma.user.aggregate({
+    _sum: {
+        age: true
     }
 })
 
-console.log(avarageAge);
+
+// count of all table data 
+const countData = await prisma.user.aggregate({
+    _count: {
+    //    _all:true,
+       age:true
+    }
+})
+
+// count of all table data another way
+const countDataAnotherWay = await prisma.user.count()
+
+
+
+// find max age
+const maxAge = await prisma.user.aggregate({
+    _max: {
+       age:true
+    }
+})
+
+
+// find min age
+const minAge = await prisma.user.aggregate({
+    _min: {
+       age:true
+    }
+})
+
+    console.log(minAge);
 }
 
 aggregates()
